73123a40c1ef65b42a62fd3d38f02b598777d25f
jerry <tangc@colorado.edu>
2016-04-18 13:43:11 -0600

    add make dep

--- geometry.c
+++ geometry.c
@@ -73,15 +73,15 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
 double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c){
     if(!a){
         DEBUG(__FILE__, __LINE__, __func__, "'a' must not be NULL");
-        return;
+        return NAN;
     }
     if(!b){
         DEBUG(__FILE__, __LINE__, __func__, "'b' must not be NULL");
-        return;
+        return NAN;
     }
     if(!c){
         DEBUG(__FILE__, __LINE__, __func__, "'c' must not be NULL");
-        return;
+        return NAN;
     }
     /* math */
     return abs(((a->x * (b->y - c->y)) + (b->x * (c->y - a->y)) + (c->x * (a->y - b->y)))/2);
